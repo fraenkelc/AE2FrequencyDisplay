78867683a8662906947d38618aee58581416d381
Christian Fraenkel <fraenkelc@users.noreply.github.com>
2015-01-31 16:19:26 +0100

    version string is now included in the mod

--- src/main/java/net/lessqq/mc/ae2freq/Constants.java
+++ src/main/java/net/lessqq/mc/ae2freq/Constants.java
@@ -3,7 +3,7 @@ package net.lessqq.mc.ae2freq;
 public interface Constants {
 
 	public static final String MODID = "AE2FrequencyDisplay";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "@version@";
 	public static final String CFG_SHOW_HEX = "ae2freq.showFrequencyHex";
 
 }
